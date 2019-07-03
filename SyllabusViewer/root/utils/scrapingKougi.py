dc2553a94e9de0c19c42c1f2444a0fa7f99358e9
ryosism <sn0wbreeze2000@yahoo.co.jp>
2019-07-03 22:44:01 +0900

    その他の情報もある程度網羅、ログインセッションがないと対象学科が抽出できない…

--- SyllabusViewer/root/utils/scrapingKougi.py
+++ SyllabusViewer/root/utils/scrapingKougi.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseURL = "https://tora-net.sti.chubu.ac.jp/syllabusv3/slbssbdr.do?value(risyunen)=2019&value(semekikn)=1&value(kougicd)={}&value(crclumcd)=01011100002019"
+baseURL = "https://tora-net.sti.chubu.ac.jp/syllabusv3/slbssbdr.do?value(risyunen)=2019&value(semekikn)=1&value(kougicd)={}&value(crclumcd)=01011300002016"
 
 # for idx in range(1):
 #     kougicd = str(idx).zfill(5)
@@ -9,20 +9,23 @@ baseURL = "https://tora-net.sti.chubu.ac.jp/syllabusv3/slbssbdr.do?value(risyune
 #     response = requests.get(URL)
 #     print(response)
 
-kougicd = str(11319).zfill(5)
+kougicd = str(12547).zfill(5)
 URL = baseURL.format(kougicd)
 response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 kougi = soup.find_all(class_="kougi")
-if len(kougi) == 0:
-    continue
 
-for label in kougi:
-    title = kougi[0].prettify().split("\n")[1].strip()
-    teacher = kougi[3].prettify().split("\n")[3].strip().replace("\u3000", " ")
-    gakunen = kougi[5].prettify().split("\n")[1].strip()
-    semester = kougi[6].prettify().split("\n")[1].strip()
-    
+# if len(kougi) == 0:
+#     continue
 
-    # tani = kougi[4].prettify().split("\n")[1].strip()
+title = kougi[0].prettify().split("\n")[1].strip()
+teacher = kougi[3].prettify().split("\n")
+teachers = [teacher.strip().replace("\u3000", "") for teacher in teacher if not "<" in teacher][:-1]
+tani = kougi[4].prettify().split("\n")[1].strip()
+gakunen = kougi[5].prettify().split("\n")[1].strip()
+semester = kougi[6].prettify().split("\n")[1].strip()
+zikan = kougi[7].prettify().split("\n")[3].split("：")[-1]
+# taisyo =
+
+import pdb; pdb.set_trace()
