e8aaa3d4bece835aeacabd6608c2e03b5003b7b6
ryosism <sn0wbreeze2000@yahoo.co.jp>
2019-07-05 15:02:20 +0900

    評価基準や概要などを整形して取得

--- SyllabusViewer/root/utils/scrapingKougi.py
+++ SyllabusViewer/root/utils/scrapingKougi.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseURL = "https://tora-net.sti.chubu.ac.jp/syllabusv3/slbssbdr.do?value(risyunen)=2019&value(semekikn)=1&value(kougicd)={}&value(crclumcd)=01011300002016"
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
 
-# for idx in range(1):
-#     kougicd = str(idx).zfill(5)
-#     URL = baseURL.format(kougicd)
-#     response = requests.get(URL)
-#     print(response)
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'syllubusviewer',
+})
+db = firestore.client()
+doc_ref = db.collection('kougis').document()
+batch = db.batch()
+
+baseURL = "https://tora-net.sti.chubu.ac.jp/syllabusv3/slbssbdr.do?value(risyunen)=2019&value(semekikn)=1&value(kougicd)={}&value(crclumcd)=01011300002016"
 
 kougicd = str(12547).zfill(5)
 URL = baseURL.format(kougicd)
@@ -28,4 +35,12 @@ semester = kougi[6].prettify().split("\n")[1].strip()
 zikan = kougi[7].prettify().split("\n")[3].split("：")[-1]
 # taisyo =
 
+others = [td for td in soup.find_all("td") if td.get("style") == "background-color:; vertical-align:middle"]
+
+# 面倒だけど前後のtdタグと/brタグを削除している、その後結合、<br>タグだけは残すか要考慮
+details = []
+for detail in others:
+    details.append("".join([content.strip() for content in detail.prettify().split("\n")[1:-2] if not "br" in content]))
+
+text, mokuhyo, houhou, hyoka, hyokakizyun = details
 import pdb; pdb.set_trace()
