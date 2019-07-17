788183af730d8aef19d868497b7427d0c29bd078
ryosism <sn0wbreeze2000@yahoo.co.jp>
2019-07-17 21:11:46 +0900

    tsファイルの更新(けど、cloudfunctionはjsで動かすから使わないかも)

--- SyllabusViewer/root/functions/src/index.ts
+++ SyllabusViewer/root/functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as algoliasearch from 'algoliasearch';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const env = functions.config();
+
+// Algolia Clientの初期化
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+// 作成したAlgoliaのIndex名を入れる
+const index = client.initIndex('SyllabusViewer');
+
+exports.insertKougi = functions.firestore
+    .document('kougis/{kougiID}')
+    .onCreate((snap, context) => {
+        const kougi = snap.data();
+
+        // kougiIDをurlの引数から拾ってくる
+        const objectID = context.params.kougiID;
+
+    return index.addObject({objectID, kougi});
+});
